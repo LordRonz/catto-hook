ae266d2c923219f121d921cf4b2597e189df7c0a
LordRonz <aarontanhar2000@gmail.com>
2022-02-26 10:07:42 +0700

    feat: allow gifs to be sent :rocket:

--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,3 @@
 def is_image(url: str):
-    extension = url[-3:].lower()
-    return "jpg" in extension or "png" in extension
+    extension = url[-4:].lower()
+    return extension in [".gif", ".jpg", ".png"]
