f3d752b015d9b23d4bd084b38ccc6a5ffcb0c400
LordRonz <aarontanhar2000@gmail.com>
2022-05-19 19:16:26 +0700

    feat: add more facts api :rocket:

--- catto/fact.py
+++ catto/fact.py
@@ -1,12 +1,17 @@
 import requests
+from random import randint
 
 
 class CatFact:
     def __init__(self, amount=10):
         self.amount = amount
         self.api_url = "https://cat-fact.herokuapp.com/"
+        self.api_url_2 = "https://catfact.ninja/"
 
     def get_fact(self):
+        if randint(0, 1) == 1:
+            return self.get_fact_2()
+
         retry = 0
         while retry < 10:
             res = requests.get(f"{self.api_url}facts/random?amount={self.amount}")
@@ -16,4 +21,9 @@ class CatFact:
                     return fact["text"]
 
             retry += 1
-        return "Not found"
+        return self.get_fact_2()
+
+    def get_fact_2(self):
+        res = requests.get(f"{self.api_url_2}fact")
+        fact = res.json()
+        return fact["fact"]
