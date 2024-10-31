ee232abae14cf3adf91251f923128ff6bedbdcf0
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-10-31 21:08:00 +0700

    fix: dead cat fact api

--- catto/fact.py
+++ catto/fact.py
@@ -10,16 +10,19 @@ class CatFact:
         self.api_url_2 = "https://catfact.ninja/"
 
     def get_fact(self):
-        if randint(0, 1) == 1:
-            return self.get_fact_2()
+        # if randint(0, 1) == 1:
+        #     return self.get_fact_2()
+        return self.get_fact_2
 
         retry = 0
         while retry < 10:
-            res = requests.get(f"{self.api_url}facts/random?amount={self.amount}")
+            res = requests.get(f"{self.api_url}facts/random?amount={self.amount}", timeout=20)
             try:
                 cat_facts = res.json()
             except requests.exceptions.JSONDecodeError:
                 continue
+            except requests.exceptions.Timeout:
+                break
             for fact in cat_facts:
                 if fact["status"]["verified"]:
                     return fact["text"]
@@ -28,6 +31,6 @@ class CatFact:
         return self.get_fact_2()
 
     def get_fact_2(self):
-        res = requests.get(f"{self.api_url_2}fact")
+        res = requests.get(f"{self.api_url_2}fact", timeout=20)
         fact = res.json()
         return fact["fact"]
